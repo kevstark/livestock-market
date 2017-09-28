d9d52200a0a980425308aa0162915efe8ef64cb5
Kev Stark <kev.stark@gmail.com>
2017-09-28 21:15:44 +1000

    Tesseract trial

--- r/02-facebook_cattlenumbers.R
+++ r/02-facebook_cattlenumbers.R
@@ -4,6 +4,6 @@ if(suppressWarnings(!require("stringr"))) { install.packages("stringr") }; libra
 
 file_paths <- list.files("../data/", "*.jpg", full.names = TRUE)
 
-ocr_text <- tesseract::ocr(file_paths[1])
+ocr_text <- tesseract::ocr("../data/sale-numbers/18425090_749456725179545_8647921519900380131_n_2.jpg")
 stringr::str_split(ocr_text, "\n")
 
