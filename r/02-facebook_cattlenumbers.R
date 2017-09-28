74577b98895dda0826577bda7ea6acbeaa5ba206
Kev Stark <kev.stark@gmail.com>
2017-09-28 20:37:57 +1000

    Revert "Dev"

--- r/02-facebook_cattlenumbers.R
+++
@@ -1,9 +0,0 @@
-
-if(suppressWarnings(!require("tesseract"))) { install.packages("tesseract") }; library(tesseract)
-if(suppressWarnings(!require("stringr"))) { install.packages("stringr") }; library(stringr)
-
-file_paths <- list.files("../data/", "*.jpg", full.names = TRUE)
-
-ocr_text <- tesseract::ocr(file_paths[1])
-stringr::str_split(ocr_text, "\n")
-
